20ab48481259afcea2a6041abd981811a0923049
Carolina Thomaz <carol.com.jp@gmail.com>
2019-01-16 00:36:04 -0200

    Routes and Rental

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderCOmponent } from './common/header/header.component';
+import { TempComponent } from './temp/temp.component';
+import { RentalComponent } from './rental/rental.component';
+
+import { RentalModule } from './rental/rental.module';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/rentals', pathMatch: 'full'},
+  {path: 'temp', component:TempComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderCOmponent
-  ],
+    HeaderCOmponent,
+    TempComponent
+   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    RentalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/rental/rental-detail/rental-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'airbnb-rental-detail',
+  templateUrl: './rental-detail.component.html',
+  styleUrls: ['./rental-detail.component.css']
+})
+export class RentalDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
